c438b11cf86c3c697b07dcaa4bacf53d170dd951
brokenankle94 <chrislee9407@gmail.com>
2025-06-09 13:34:22 +0900

    워닝 메세지 업데이트

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ st.sidebar.markdown("""
 본 서비스는 AI를 활용하여 다양한 종류의 PDF를 세부분석 할 수 있게 도와주는 AI 도구 입니다.
 * Gemini 1.5 flash model을 사용하고 있어 답변 생성 속도가 느릴 수 있습니다.
 """)
-st.sidebar.markdown('<p style="color: red; font-size: 0.8em;">본 AI가 제공하는 답변은 참고용이며, 정확성을 보장할 수 없습니다. 보안을 위해 회사 기밀, 개인정보등은 제공하지 않기를 권장드리며, 반드시 실제 업무에 적용하기 전에 검토하시길 바랍니다.</p>', unsafe_allow_html=True)
+st.sidebar.markdown('<p style="color: red; font-size: 0.8em;">(주의)본 AI가 제공하는 답변은 참고용이며, 정확성을 보장할 수 없습니다. 보안을 위해 회사 기밀, 개인정보등은 제공하지 않기를 권장드리며, 반드시 실제 업무에 적용하기 전에 검토하시길 바랍니다.</p>', unsafe_allow_html=True)
 st.sidebar.markdown("### 타 Link")
 st.sidebar.markdown("[개발자 링크드인](https://www.linkedin.com/in/chrislee9407/)")
 st.sidebar.markdown("[K-계리 AI 플랫폼](https://chrischangminlee.github.io/K_Actuary_AI_Agent_Platform/)")
